f3b7fafc1c5d2ed0c7a421f9e346c59b59932b53
sunho001215 <sunho001215@gmail.com>
2020-10-28 01:12:17 +0900

    update frenet_optimal_trajectory code

--- path_planning/include/quintic_polynomial.h
+++ path_planning/include/quintic_polynomial.h
@@ -1,13 +1,12 @@
-#ifndef QUINTIC_POLYNOMIAL_H
-#define QUINTIC_POLYNOMIAL_H
+#ifndef FRENET_OPTIMAL_TRAJECTORY_QUINTIC_POLYNOMIAL
+#define FRENET_OPTIMAL_TRAJECTORY_QUINTIC_POLYNOMIAL
 
 class QuinticPolynomial{
     private:
         double a0, a1, a2, a3, a4, a5;
-        
     public:
         QuinticPolynomial() = default;
-        QuinticPolynomial(double xs, double vxs, double axs, double vxe, double axe, double t);
+        QuinticPolynomial(double xs, double vxs, double axs, double xe, double vxe, double axe, double t);
         double calc_point(double t);
         double calc_first_derivative(double t);
         double calc_second_derivative(double t);
