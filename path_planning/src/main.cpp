319b46eb8ff1a1843db616d19e0a50e60a80f99b
sunho001215 <sunho001215@gmail.com>
2020-10-29 20:10:18 +0900

    update path_planning visulize

--- path_planning/src/main.cpp
+++ path_planning/src/main.cpp
@@ -3,20 +3,22 @@
 #include "cpp_struct.h"
 #include "ros/ros.h"
 #include "opencv2/opencv.hpp"
+#include <ctime>
 
 #include <iostream>
 
 using namespace std;
+using namespace cv;
 
 int VISUALIZE_IMAGE_SIZE = 200;
-int IMAGE_SCALE = 3;
+double IMAGE_SCALE = 3.0;
 int IMAGE_SIZE = VISUALIZE_IMAGE_SIZE * IMAGE_SCALE;
 
 int main(int argc, char** argv) {
     ros::init(argc, argv, "frenet_optimal_trejectory_main");
 
     double wx [3] = {0, 50, 150};
-    double wy [3] = {0, 10, 0};
+    double wy [3] = {0, 0, 0};
     double o_llx[4] = {48.0, 98.0, 98.0, 128.0};
     double o_lly[4] = {-2.0, -4.0, 6.0, 2.0};
     double o_urx[4] = {52.0, 102.0, 102.0, 132.0};
@@ -62,43 +64,53 @@ int main(int argc, char** argv) {
     ros::param::get("/klat", fot_hp.klat);
     ros::param::get("/klon", fot_hp.klon);
 
-    FrenetOptimalTrajectory fot = FrenetOptimalTrajectory(&fot_ic, &fot_hp);
-    FrenetPath* best_frenet_path = fot.getBestPath();
-    CubicSpline2D* csp = fot.getcsp();
+    while(1){
+        clock_t startTime = clock();
+        
+        FrenetOptimalTrajectory fot = FrenetOptimalTrajectory(&fot_ic, &fot_hp);
+        FrenetPath* best_frenet_path = fot.getBestPath();
+        CubicSpline2D* csp = fot.getcsp();
+        
+        clock_t endTime = clock();
+        clock_t elapsed = endTime - startTime;
+        double timeInSecond = (double)(elapsed / CLOCKS_PER_SEC);
 
-    if (best_frenet_path) {
-        cout << "Success\n";
-    }
-    else{
-        cout << "Failure\n";
-    }
-
-
-    // Visualize
-    
-    cv::Mat VisWindow(IMAGE_SIZE, IMAGE_SIZE, CV_8UC3, cv::Scalar(255, 255, 255));
-
-    
-    for (int i=0; i<fot_ic.no; i++){
-        cv::rectangle(VisWindow, cv::Rect(IMAGE_SCALE * o_llx[i],IMAGE_SIZE/2 - IMAGE_SCALE * o_ury[i], IMAGE_SCALE * (o_urx[i]-o_llx[i]), IMAGE_SCALE * (o_ury[i] - o_lly[i])), cv::Scalar(255, 0, 0));
-    }
-
-    double t_s = csp->calc_s_length();
-
-    for (double t=0; t<t_s; t+=0.5){
-        if(csp->calc_x(t) != NAN){
-            VisWindow.at<uchar>(IMAGE_SIZE/2 - int(IMAGE_SCALE*csp->calc_y(t)), int(IMAGE_SCALE*csp->calc_x(t))) = (0, 0, 0);
-            VisWindow.at<uchar>(IMAGE_SIZE/2 - int(IMAGE_SCALE*csp->calc_y(t)) + 1, int(IMAGE_SCALE*csp->calc_x(t))) = (0, 0, 0);
-            VisWindow.at<uchar>(IMAGE_SIZE/2 - int(IMAGE_SCALE*csp->calc_y(t)) - 1, int(IMAGE_SCALE*csp->calc_x(t))) = (0, 0, 0);
+        if (best_frenet_path) {
+            cout << "Success\n";
         }
+        else{
+            cout << "Failure\n";
+        }
+
+        fot_ic.s0 = best_frenet_path->s[2];
+        fot_ic.c_speed = best_frenet_path->s_d[2];
+        fot_ic.c_d = best_frenet_path->d[2];
+        fot_ic.c_d_d = best_frenet_path->d_d[2];
+        fot_ic.c_d_dd = best_frenet_path->d_dd[2];
+
+
+        cv::Mat VisWindow(IMAGE_SIZE, IMAGE_SIZE, CV_8UC3, cv::Scalar(255, 255, 255));
+
+        for (int i=0; i<fot_ic.no; i++){
+            cv::rectangle(VisWindow, cv::Rect(IMAGE_SCALE * o_llx[i],IMAGE_SIZE/2 - IMAGE_SCALE * o_ury[i], IMAGE_SCALE * (o_urx[i]-o_llx[i]), IMAGE_SCALE * (o_ury[i] - o_lly[i])), cv::Scalar(255, 0, 0));
+        }
+
+        double t_s = csp->calc_s_length();
+
+        for (double t=0; t<t_s; t+=0.5){
+            if(csp->calc_x(t) != NAN){
+                VisWindow.at<Vec3b>(int(IMAGE_SIZE/2 - IMAGE_SCALE*csp->calc_y(t)), int(IMAGE_SCALE*csp->calc_x(t))) = (0, 0, 0);
+                VisWindow.at<Vec3b>(int(IMAGE_SIZE/2 - IMAGE_SCALE*csp->calc_y(t) + 1), int(IMAGE_SCALE*csp->calc_x(t))) = (0, 0, 0);
+                VisWindow.at<Vec3b>(int(IMAGE_SIZE/2 - IMAGE_SCALE*csp->calc_y(t) - 1), int(IMAGE_SCALE*csp->calc_x(t))) = (0, 0, 0);
+            }
+        }
+
+        for (int i=0; i<best_frenet_path->x.size(); i++){
+            cv::circle(VisWindow, cv::Point(IMAGE_SCALE * int(best_frenet_path->x[i]),IMAGE_SIZE/2 - int(IMAGE_SCALE*best_frenet_path->y[i])),5, cv::Scalar(0, 0, 255));
+        }
+
+        cv::imshow("Path_Visualize", VisWindow);
+        cv::waitKey(1);
     }
-
-    for (int i=0; i<best_frenet_path->x.size(); i++){
-        cv::circle(VisWindow, cv::Point(IMAGE_SCALE * int(best_frenet_path->x[i]),IMAGE_SIZE/2 - int(IMAGE_SCALE*best_frenet_path->y[i])),5, cv::Scalar(0, 0, 255));
-    }
-
-    cv::imshow("Path_Visualize", VisWindow);
-    cv::waitKey(0);
-
     return 0;
 }
